05d3211f7947a6fa5700a31f2607f30756df67fe
姜祎楠 <jiangyinan@jiangyinandeMacBook-Pro.local>
2018-02-09 14:36:56 +0100

    php file

--- header.php
+++ header.php
@@ -1,82 +1,77 @@
 
 
      <style type="text/css">
-    .navigation{
-/*      padding-top: 30px;
-      padding-bottom: 30px;*/
-      width: 100%;
-      z-index: 1;
-      line-height: 61px;
-      padding:0 2%;
-      position: fixed;
-      top: 0;
-    }
-    .page-logo{
-      margin-top: 0px;
-      float: left;
+   .navigation {
+        /*      padding-top: 30px; */
+      padding-bottom: 30px;
+        background-color: #2e2e3e;
+        box-shadow: 0px 1px 6px 3px rgba(0, 0, 0, 0.15);
+        font-size: 14px;
+        font-weight: 600;
 
+        width: 100%;
+        z-index: 1;
+        line-height: 61px;
+        padding: 5px 25px;
+        position: fixed;
+        top: 0;
     }
 
-    .navbar-right{
-      float:right;
+    .page-logo {
+        margin-top: 0px;
+        float: left;
     }
-    .navbar-right a{
-      text-decoration: none;
-      padding:10px;
-      color: #fff;
-      font-family: Arial;
-      font-weight: 900;
-    }
-     .navbarlink:hover{
-      text-decoration: underline;
-    }
-    .navbarlogo{
-      text-decoration: none;
-      font-family: Arial;
-      font-weight: 900;
-      line-height: 50px;
-    }
-     .navbarlogo:hover{
-      text-decoration: underline;
-    }
-    .video-container{
-      z-index: -100;
-      width:100%;
-      height: 75%;
-      overflow:hidden;
-      position: absolute;
-      top:-300;
-      left:0;
-      margin-right: 0px;
-      margin-top: -200px;
-    }
-    #myvideo{
-      width: 100%;
 
+    .navbar-right {
+        float: right;
     }
+
+    .navbar-right ul {
+        margin-bottom: 0px;
+    }
+
+    .navbar-right a {
+        text-decoration: none;
+        padding: 10px;
+        font-family: Arial;
+        font-weight: 900;
+        color: #fff;
+    }
+
+    .navbarlink:hover {
+        color: #000;
+    }
+
+    .navbarlogo {
+        text-decoration: none;
+        font-family: Arial;
+        font-weight: 900;
+        font-size: 20px;
+        line-height: 50px;
+        color: #2CF3D4;
+    }
+
+    .navbarlogo:hover {
+        text-decoration: underline;
+    }
+
   </style>
 
-     <header>
-          <div class="header">
+    <header>
+        <div class="header">
             <nav class="navigation">
-              <img src="image/LogoRobot_flowint.dk_.gif" class="page-logo" width="60px">
-              <a href="#" class="navbarlogo">FlowINT</a>
-              <div class="navbar-right">
-                <ul>
-                    <a href="#" class="navbarlink">About me</a>
-                    <a href="#" class="navbarlink">Work</a>
-                    <a href="#" class="navbarlink">Contact</a>
-                    
-                </ul>
-              </div>
+                <img src="image/LogoRobot_flowint.dk_.gif" class="page-logo" width="60px">
+                <a href="index.html" class="navbarlogo">FlowINT</a>
+                <div class="navbar-right">
+                    <ul>
+                        <a href="Flowint.html" class="navbarlink">what is FlowINT</a>
+                        <a href="#aboutme" class="navbarlink">solutions</a>
+                        <a href="contact.html" class="navbarlink">Contact</a>
+                    </ul>
+                </div>
             </nav>
-            <!-- <div class="video-container">
-              <video autoplay loop muted id="myvideo">
-                <source src="video/background.mp4" type="video/mp4">
-              </video> 
-            </div>  -->
-          </div>
-        </header>
+        </div>
+    </header>
   
 
 
